db50b2840f9a351a33415001c8b2dbdcdf393ea9
Kain Nhantumbo <nhantumbok@gmail.com>
2023-12-08 01:20:43 +0200

    feat: added api cloud url

--- src/config/client.ts
+++ src/config/client.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.API_BASE_URL || 'http://localhost:4500';
+// const API_BASE_URL = process.env.API_BASE_URL || 'http://localhost:4500';
+const API_BASE_URL = 'https://rubymart-sales-api.onrender.com';
 
 export default axios.create({ baseURL: API_BASE_URL });
